460c8b16ab20fd6b12b7be462c47187d8fb781bb
Afsah Siddiqui <afsahasad90@gmail.com>
2026-04-21 21:29:00 -0400

    implement dynamic account selection to reduce test flakiness

--- automated-testing/tests/test_new_account_creation.py
+++ automated-testing/tests/test_new_account_creation.py
@@ -2,12 +2,18 @@ from pages.accounts_overview_page import AccountsOverview
 
 def test_new_account_creation(logged_in_driver, config, logger):
     
-    accounts_overview = AccountsOverview(logged_in_driver) 
+    accounts_overview = AccountsOverview(logged_in_driver)
+
+    logger.info("Fetching real-time account IDs...")
+    existing_ids = accounts_overview.get_all_accounts_ids()
+
+    active_id = existing_ids[0] 
+    logger.info(f"Using dynamic Account ID: {active_id}") 
 
     logger.info("Opening New Account...")
     open_new_acc_page = accounts_overview.select_service("open new account")
 
-    open_new_acc_page.open_new_account(config["ACCOUNT_TYPE"], config["ACCOUNT_ID"])
+    open_new_acc_page.open_new_account(config["ACCOUNT_TYPE"], active_id)
 
     logger.info("Navigating to Accounts Overview...")
     accounts_overview = open_new_acc_page.select_service("Accounts overview")
